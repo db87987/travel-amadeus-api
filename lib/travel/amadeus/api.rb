3457b734bf8e2f170a88ab6642c27e1e9ea1f02e
Dragon Black <db87987@gmail.com>
2017-07-11 11:45:13 +0300

    Initial commit

---
+++ lib/travel/amadeus/api.rb
@@ -0,0 +1,9 @@
+require "travel/amadeus/api/version"
+
+module Travel
+  module Amadeus
+    module Api
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/travel/amadeus/api/version.rb
@@ -0,0 +1,7 @@
+module Travel
+  module Amadeus
+    module Api
+      VERSION = "0.1.0"
+    end
+  end
+end
